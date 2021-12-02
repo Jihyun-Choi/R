c407bfabce77554dd22f5aed77348458bd066eac
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-12-03 03:17:26 +0900

    Homework_2 HypothesisTest 211202

--- Homework_2_211202.R
+++ Homework_2_211202.R
@@ -7,7 +7,7 @@
 
 setRepositories(ind = 1:7)
 
-install.packages("data.table")
+#install.packages("data.table")
 library(data.table)
 
 ######################################################
@@ -18,34 +18,42 @@ WORK_DIR <- "C:\\Users\\user\\HW"
 setwd(WORK_DIR)
 getwd()
 
-
-Data <- data.frame(fread("Data.txt", sep = "\t", head=T, stringAsFactors = T))
+Data <- data.frame(fread("Data.txt", sep = "\t", head=T, stringsAsFactors = T))
 class(Data)
+
 dim(Data)
 str(Data)
 
 double[][] = 
-idx <- which(Data$Disease == "COVID19")
+  idx <- which(Data$Disease == "COVID19")
 filteredData <- Data[idx,]
 
-View(Data)
+dim(filteredData)
+nrow(filteredData)
+View(filteredData)
 
-############################
+x_bar = mean(filteredData$sysBP)
+x_bar 
+n = nrow(filteredData)
+mu = 127.5
+var = 16.5^2
+
+###########################################################
 z.test <- function(input, mu, var){
   z <- (mean(input) - mu) / (sqrt(var / length(input)))
   return(z)
 }
 
+# Get Z-statistic
+Zstat <- z.test(filteredData$sysBP, mu=127.5, var=16.5^2)
 
+
+# Q1
 2*pnorm(-abs(Zstat)) # two-sided Z-test
 
+# Q2
+1-pnorm(Zstat) # one-sided Z-test (Z >= COVID19&sysBP)
 
-
-
-
-
-
-
-
-
+# Q3
+pnorm(Zstat) # one-sided Z-test (Z <= COVID19&sysBP)
 
